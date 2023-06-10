d6650118237a803d7692f0d396d6228567a59d5d
mrrhy <rhy159753@gmail.com>
2023-06-10 11:46:53 +0800

    connect API

--- lib/model/env.dart
+++ lib/model/env.dart
@@ -1,4 +1,4 @@
 class Env {
-  // static const String API_URL = 'https://api.bamservices.mrrhy.org/';
-  static const String API_URL = 'http://localhost/bamservices/';
+  static const String API_URL = 'https://api.bamservices.mrrhy.org/';
+  // static const String API_URL = 'http://localhost/bamservices/';
 }
